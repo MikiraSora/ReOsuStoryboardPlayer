d7e038cbdecc39959b6a8502449b56413aead36d
Mikira Sora <mikirasora0409@126.com>
2017-08-25 17:55:32 +0800

    add test project

---
+++ Test/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleRenderFramework;
+
+namespace Test
+{
+	public class Test
+	{        
+		public static void Main(string[] argv)
+		{
+            TestGameWindow s = new TestGameWindow();
+
+            Engine.Debug = false;
+
+            s.resizeWindow(640, 480);
+            s.Run();
+        }
+	}
+}
---
+++ Test/TestGameWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleRenderFramework;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Test
+{
+	public class TestGameWindow : Window
+	{
+        public ReOsuStoryBoardPlayer.StoryBoardInstance instance;
+
+        protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+            #region InitGraphics
+
+            GL.DepthRange(0, 100);
+
+            #endregion
+
+            instance = new ReOsuStoryBoardPlayer.StoryBoardInstance(@"H:\SBTest\372552 yuiko - Azuma no Sora kara Hajimaru Sekai (Short)");
+
+            instance.Start();
+
+            Title = "Esu!StoryBoardPlayer";
+
+            VSync = VSyncMode.Off;
+
+            Engine.LoadSystem(new ReOsuStoryBoardPlayer.StoryboardRenderSystem(instance));
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);          
+
+            instance.Update();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+        }
+    }
+}
