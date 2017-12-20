3a559ad793b3c882bdf544ec8f5afaa52a52b730
Mikira Sora <mikirasora0409@126.com>
2017-12-20 22:27:13 +0800

    delete unused Texture methods

--- ReOsuStoryBoardPlayer/Graphics/Texture.cs
+++ ReOsuStoryBoardPlayer/Graphics/Texture.cs
@@ -52,30 +52,6 @@ namespace ReOsuStoryBoardPlayer
             LoadFromFile(fname);
         }
 
-        public Texture(byte[] bytes)
-        {
-            name = "Texture";
-            _id = 0;
-
-            LoadFromByteArray(bytes);
-        }
-
-        public Texture(Bitmap bitmap)
-        {
-            name = "Texture";
-            _id = 0;
-
-            LoadFromBitmap(bitmap);
-        }
-
-        public Texture(int id, int width, int height)
-        {
-            name = "Texture";
-            _id = 0;
-
-            LoadFromTextureId(id, width, height);
-        }
-
         public void LoadFromFile(string filename)
         {
             if (!String.IsNullOrEmpty(filename))
@@ -107,75 +83,6 @@ namespace ReOsuStoryBoardPlayer
             }
         }
 
-        public void LoadFromBitmap(Bitmap bmp)
-        {
-            _filename = filePath;
-
-            GL.GenTextures(1, out _id);
-
-            GL.BindTexture(TextureTarget.Texture2D, _id);
-
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
-            /*
-            using (MemoryStream ms = new MemoryStream())
-            {
-                bmp.Save(ms, ImageFormat.Png);
-                _bytes = ms.ToArray();
-                ms.Dispose();
-            }
-            */
-            //bitmap = bmp;
-
-            _textureSize = new Vector(bmp.Width, bmp.Height);
-
-            BitmapData bmp_data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bmp_data.Width, bmp_data.Height, 0,
-                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, bmp_data.Scan0);
-
-            bmp.UnlockBits(bmp_data);
-        }
-
-        public void LoadFromByteArray(byte[] fbytes)
-        {
-            if (fbytes != null)
-            {
-                //var _bytes = fbytes;
-                GL.GenTextures(1, out _id);
-
-                GL.BindTexture(TextureTarget.Texture2D, _id);
-
-                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
-                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
-
-                MemoryStream ms = new MemoryStream();
-                ms.Write(fbytes, 0, fbytes.Length);
-                ms.Seek(0, SeekOrigin.Begin);
-                Bitmap bmp = new Bitmap(ms);
-                ms.Dispose();
-
-                //bitmap = bmp;
-                _textureSize = new Vector(bmp.Width, bmp.Height);
-
-                BitmapData bmp_data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-
-                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bmp_data.Width, bmp_data.Height, 0,
-                    OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, bmp_data.Scan0);
-
-                bmp.UnlockBits(bmp_data);
-
-                bmp.Dispose();
-            }
-        }
-
-        public void LoadFromTextureId(int texGenId, int width, int height)
-        {
-            _id = texGenId;
-
-            _textureSize = new Vector(width, height);
-        }
-
         public void LoadFromData(IntPtr data, int width, int height)
         {
             if (data != null)
@@ -202,6 +109,6 @@ namespace ReOsuStoryBoardPlayer
         public void Dispose()
         {
             GL.DeleteTexture(_id);
-        }
+        }   
     }
 }
