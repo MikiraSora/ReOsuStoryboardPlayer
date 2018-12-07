e5e1d1e95e46efb275f5532f51ae88a400a4ec73
Mikira Sora <mikirasora0409@126.com>
2018-12-07 11:21:17 +0800

    add SerializeDecodeStoryboardContent()

--- ReOsuStoryBoardPlayer/MainProgram.cs
+++ ReOsuStoryBoardPlayer/MainProgram.cs
@@ -8,6 +8,10 @@ using System.Linq;
 using ReOsuStoryBoardPlayer.DebugTool.Debugger.CLIController;
 using ReOsuStoryBoardPlayer.Parser.Extension;
 using System.Runtime.InteropServices;
+using ReOsuStoryBoardPlayer.Parser.Stream;
+using ReOsuStoryBoardPlayer.Parser.Collection;
+using ReOsuStoryBoardPlayer.Parser.Reader;
+using System.IO;
 
 namespace ReOsuStoryBoardPlayer
 {
@@ -57,12 +61,12 @@ namespace ReOsuStoryBoardPlayer
             window.Run();
         }
 
-        private static void ParseProgramCommands(string[] argv, out int w, out int h,out string beatmap_folder)
+        private static void ParseProgramCommands(string[] argv, out int w, out int h, out string beatmap_folder)
         {
             //default 
             w=1600;
             h=900;
-            beatmap_folder = @"G:\SBTest\237977 marina - Towa yori Towa ni";
+            beatmap_folder=@"G:\SBTest\839266 Jeremy Blake - Flex";
 
             var sb = new ArgParser(new ParamParserV2('-', '\"', '\''));
             var args = sb.Parse(argv);
@@ -71,31 +75,83 @@ namespace ReOsuStoryBoardPlayer
             {
                 if (args.FreeArgs!=null)
                     beatmap_folder=args.FreeArgs.FirstOrDefault()??beatmap_folder;
-                
+
                 if (args.TryGetArg(out var valW, "width", "w"))
                     w=int.Parse(valW);
 
                 if (args.TryGetArg(out var valH, "height", "h"))
                     h=int.Parse(valH);
 
-                if (args.TryGetArg(out var folder, "folder","f"))
+                if (args.TryGetArg(out var folder, "folder", "f"))
                     beatmap_folder=folder;
-                
-                if (args.TryGetArg(out var p_update_limit, "-parallel_update_limit","-pu"))
+
+                if (args.TryGetArg(out var p_update_limit, "-parallel_update_limit", "-pu"))
                     Setting.ParallelUpdateObjectsLimitCount=p_update_limit.ToInt();
-                
+
                 if (args.TryGetArg(out var p_parse_limit, "-parallel_parse_limit", "-pp"))
                     Setting.ParallelParseCommandLimitCount=p_parse_limit.ToInt();
 
                 Setting.MiniMode=args.Switches.Any(k => k=="mini");
                 Setting.EnableSplitMoveScaleCommand=args.Switches.Any(k => k=="enable_split");
                 Setting.EnableRuntimeOptimzeObjects=args.Switches.Any(k => k=="enable_runtime_optimze");
+
+                //额外功能
+                if (args.TryGetArg(out var parse_type, "parse"))
+                {
+                    var parse_osb = parse_type=="osb";
+                    args.TryGetArg(out var output_path, "parse_output");
+                    SerializeDecodeStoryboardContent(beatmap_folder, parse_osb, output_path);
+                }
             }
         }
 
+        #region ProgramCommands
+
+        //将解析好的内容再序列化成osb文件格式的文本内容
+        private static void SerializeDecodeStoryboardContent(string beatmap_folder,bool parse_osb,string output_path)
+        {
+            try
+            {
+                var info = BeatmapFolderInfo.Parse(beatmap_folder);
+                var input_file = parse_osb ? info.osb_file_path : info.osu_file_path;
+                output_path=string.IsNullOrWhiteSpace(output_path) ? input_file+".parse_output" : output_path;
+
+                Log.User($"Start serialize {input_file} ....");
+                using (var writer = new StreamWriter(File.OpenWrite(output_path)))
+                {
+                    //先塞个头
+                    writer.WriteLine($"[{Section.Events.ToString()}]");
+
+                    OsuFileReader reader = new OsuFileReader(input_file);
+
+                    VariableCollection collection = new VariableCollection(new VariableReader(reader).EnumValues());
+
+                    SectionReader section = new SectionReader(Section.Events, reader);
+
+                    EventReader event_section = new EventReader(reader, collection);
+
+                    foreach (var line in section.EnumValues())
+                    {
+                        var decode_line = event_section.LineProcessVariable(line);
+                        writer.WriteLine(decode_line);
+                    }
+                }
+
+                Log.User("Serialize successfully! it output to "+output_path);
+                Exit("Serialize successfully! it output to "+output_path);
+            }
+            catch (Exception e)
+            {
+                Log.Error("Serialize failed!"+e.Message);
+                Exit("Serialize failed!"+e.Message);
+            }
+        }
+
+        #endregion
+
         private static void Exit(string reason)
         {
-            if (!Setting.MiniMode)
+            if ((!Setting.MiniMode)&&(!string.IsNullOrWhiteSpace(reason)))
             {
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.ForegroundColor = ConsoleColor.Yellow;
