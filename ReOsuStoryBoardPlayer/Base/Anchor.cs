c1c184f7fd3f18ced5994e2266ceee1f65e3dc9d
milkitic <milkitic@126.com>
2018-12-08 18:03:18 +0800

    simple fix anchor

--- ReOsuStoryBoardPlayer/Base/Anchor.cs
+++ ReOsuStoryBoardPlayer/Base/Anchor.cs
@@ -2,14 +2,14 @@
 {
     public enum Anchor
     {
-        TopLeft,
-        TopCentre,
-        TopRight,
-        CentreLeft,
-        Centre,
-        CentreRight,
-        BottomLeft,
-        BottomCentre,
-        BottomRight
+        TopLeft = 6,
+        TopCentre = 5,
+        TopRight = 3,
+        CentreLeft = 2,
+        Centre = 1,
+        CentreRight = 7,
+        BottomLeft = 8,
+        BottomCentre = 4,
+        BottomRight = 9,
     }
 }
