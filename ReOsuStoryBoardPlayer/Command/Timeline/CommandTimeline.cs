bf1be5dd809a42d321350cb896bb628f8ab89453
Mikira Sora <mikirasora0409@126.com>
2018-12-10 22:42:23 +0800

    clean code

--- ReOsuStoryBoardPlayer/Command/Timeline/CommandTimeline.cs
+++ ReOsuStoryBoardPlayer/Command/Timeline/CommandTimeline.cs
@@ -25,37 +25,31 @@ namespace ReOsuStoryBoardPlayer.Commands
         {
             Debug.Assert(!float.IsNaN(current_time), $"current_time is not a number");
 
-            Command command = null;
-
             //cache
-            if (selected_command != null && (selected_command.StartTime <= current_time && current_time <= selected_command.EndTime))
+            if (selected_command!=null&&(selected_command.StartTime<=current_time&&current_time<=selected_command.EndTime))
                 return selected_command;
 
-            if (current_time < this.First().StartTime)
-                return selected_command = this.First();
-            else if (current_time > this.Last().EndTime)
-                return selected_command = this.Last();
+            if (current_time<this.First().StartTime)
+                return selected_command=this.First();
+            else if (current_time>this.Last().EndTime)
+                return selected_command=this.Last();
 
             //尝试选取在时间范围内的命令
-            if (command == null)
-                foreach (var cmd in this)
-                    if (current_time >= cmd.StartTime && current_time <= cmd.EndTime)
-                        return selected_command = cmd;
+            foreach (var cmd in this)
+                if (current_time>=cmd.StartTime&&current_time<=cmd.EndTime)
+                    return selected_command=cmd;
 
             //尝试选取在命令之间的前者
-            if (command == null)
-                for (int i = 0; i < Count - 1; i++)
-                {
-                    var cur_cmd = this[i];
-                    var next_cmd = this[i + 1];
+            for (int i = 0; i<Count-1; i++)
+            {
+                var cur_cmd = this[i];
+                var next_cmd = this[i+1];
 
-                    if (current_time >= cur_cmd.EndTime && current_time <= next_cmd.StartTime)
-                    {
-                        return selected_command = cur_cmd;
-                    }
-                }
+                if (current_time>=cur_cmd.EndTime&&current_time<=next_cmd.StartTime)
+                    return selected_command=cur_cmd;
+            }
 
-            return selected_command = null;
+            return selected_command=null;
         }
     }
 }
