a8ee4450af141351c83e0d8e7bcad7352afaaa58
Mikira Sora <mikirasora0409@126.com>
2018-12-12 17:22:01 +0800

    implement useless CombineCommands

--- ReOsuStoryBoardPlayer/Optimzer/Runtime/RuntimeStoryboardOptimzer.cs
+++ ReOsuStoryBoardPlayer/Optimzer/Runtime/RuntimeStoryboardOptimzer.cs
@@ -14,6 +14,12 @@ namespace ReOsuStoryBoardPlayer.Optimzer.Runtime
             int effect_count = 0;
             TrimFrameTime(storyboard_objects,ref effect_count);
             Log.User("TrimFrameTime() optimze count:"+effect_count);
+
+            /*
+            effect_count=0;
+            CombineCommands(storyboard_objects, ref effect_count);
+            Log.User("CombineCommands() optimze count:"+effect_count);
+            */
         }
 
         public void TrimFrameTime(IEnumerable<StoryBoardObject> storyboard_objects,ref int effect_count)
@@ -37,30 +43,59 @@ namespace ReOsuStoryBoardPlayer.Optimzer.Runtime
             }
         }
 
-        /* ValueCommand<VALUE_TYPE> 我敲里吗
-        public void CombineCommands(IEnumerable<StoryBoardObject> storyboard_objects)
+        public void CombineCommands(IEnumerable<StoryBoardObject> storyboard_objects, ref int effect_count)
         {
             foreach (var obj in storyboard_objects)
             {
                 foreach (var pair in obj.CommandMap)
                 {
-                    var timeline = pair.Value.OfType<ValueCommand>();
+                    var real_timeline = pair.Value;
 
-                    for (int i = 0; i<timeline.Count()-1; i++)
+                    //立即求值
+                    var normal_timeline = pair.Value.OfType<ValueCommand>().ToArray();
+
+                    if (normal_timeline.Length==0)
+                        continue;
+
+                    //ValueCommand<TYPE_VALUE>我敲里吗
+                    var type = normal_timeline.First().GetType();
+                    var end_value_prop = type.GetProperty("EndValue");
+                    var start_value_prop = type.GetProperty("StartValue");
+
+                    for (int i = 0; i<normal_timeline.Count()-1; i++)
                     {
-                        var cmd = timeline.ElementAt(i);
-                        var next_cmd = timeline.ElementAt(i+1);
+                        var cmd = normal_timeline[i];
+                        var next_cmd = normal_timeline[i+1];
                         
                         if ((cmd.Easing==next_cmd.Easing)
                             &&(cmd.EndTime==next_cmd.StartTime)
-                            &&(cmd.EndValue==next_cmd.StartValue))
+                            &&(end_value_prop.GetValue(cmd)==start_value_prop.GetValue(next_cmd)))
                         {
                             //combine
+                            var new_cmd = (ValueCommand)type.Assembly.CreateInstance(type.FullName);
+
+                            new_cmd.EndTime=next_cmd.EndTime;
+                            new_cmd.StartTime=cmd.StartTime;
+                            new_cmd.Easing=cmd.Easing;
+                            end_value_prop.SetValue(new_cmd, end_value_prop.GetValue(next_cmd));
+                            start_value_prop.SetValue(new_cmd, start_value_prop.GetValue(cmd));
+
+                            //remove old
+                            var index = real_timeline.IndexOf(cmd);
+                            real_timeline.Remove(cmd);
+                            real_timeline.Remove(next_cmd);
+
+                            //insert new
+                            real_timeline.Insert(index, new_cmd);
+
+                            //skip next command
+                            i++;
+
+                            effect_count++;
                         }
                     }
                 }
             }
         }
-        */
     }
 }
