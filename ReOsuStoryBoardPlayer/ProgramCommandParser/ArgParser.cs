3f8120e48dad57336cb7b20264e3a5aab5fcb16b
Mikira Sora <mikirasora0409@126.com>
2018-12-26 12:32:36 +0800

    fixed ArgParser::Parse()

--- ReOsuStoryBoardPlayer/ProgramCommandParser/ArgParser.cs
+++ ReOsuStoryBoardPlayer/ProgramCommandParser/ArgParser.cs
@@ -20,5 +20,19 @@ namespace ReOsuStoryBoardPlayer.ProgramCommandParser
 
             return null;
         }
+
+        public Parameters Parse(string[] args)
+        {
+            for (var i = 0; i<args.Length; i++)
+            {
+                var s = args[i];
+                if (s.Any(k => k==' '))
+                {
+                    args[i]=$"{_parser.Quotes.First()}{s}{_parser.Quotes.First()}";
+                }
+            }
+
+            return Parse(string.Join(" ", args));
+        }
     }
 }
