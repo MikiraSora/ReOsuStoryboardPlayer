f993b976d6b7b74961c310d757b2b2250aac0e8f
Mikira Sora <mikirasora0409@126.com>
2019-12-20 12:58:57 +0800

    fix RemoveUnusedCommand() condition

--- ReOsuStoryboardPlayer.Core/Optimzer/DefaultOptimzer/ParserStaticOptimzer.cs
+++ ReOsuStoryboardPlayer.Core/Optimzer/DefaultOptimzer/ParserStaticOptimzer.cs
@@ -102,7 +102,6 @@ namespace ReOsuStoryboardPlayer.Core.Optimzer.DefaultOptimzer
                         for (int i = 0; timeline.Overlay&&i<timeline.Count-1; i++)
                         {
                             var cmd = timeline[i];
-
                             /*
                              *(cmd)      : |--------------------------|
                              *(next_cmd) :             |--------------|       <--- Killed , biatch
@@ -111,13 +110,18 @@ namespace ReOsuStoryboardPlayer.Core.Optimzer.DefaultOptimzer
                             {
                                 var next_cmd = timeline[t];
 
+                                if (next_cmd.RelativeLine == 170726)
+                                {
+
+                                }
+
                                 if (next_cmd.StartTime>cmd.EndTime)
                                     break;
 
                                 if (
                                 next_cmd.EndTime<=cmd.EndTime &&
                                 next_cmd.EndTime!=next_cmd.StartTime &&
-                                cmd.CompareTo(next_cmd)<0)
+                                cmd.CompareTo(next_cmd)<=0)
                                 {
                                     timeline.Remove(next_cmd);
 
--- ReOsuStoryboardPlayer/MainProgram.cs
+++ ReOsuStoryboardPlayer/MainProgram.cs
@@ -137,7 +137,7 @@ namespace ReOsuStoryboardPlayer
 
         private static Parameters ParseProgramCommands(string[] argv, out string beatmap_folder)
         {
-            beatmap_folder= @"G:\SBTest\548679 DJ NAGAI feat a_ru - Benibotan";
+            beatmap_folder= @"G:\SBTest\381480 Halozy - Itsuka Furu Ame, Sakura";
 
             var sb = new ArgParser(new ParamParserV2('-', '\"'));
             var args = sb.Parse(argv);
